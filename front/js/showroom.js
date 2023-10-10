a975a6c600345166d7a4a5159a9a3edafbfe6842
Del <deldelnd02@gmail.com>
2023-10-10 22:56:06 +0100

    page showroom + page details

--- front/js/showroom.js
+++ front/js/showroom.js
@@ -1,3 +1,8 @@
 $(document).ready(async function () {
   console.log("showroom.js Loaded");
 });
+
+$('.card').click(function (e) { 
+  e.preventDefault();
+  window.location.href = 'car-details.html'
+});
