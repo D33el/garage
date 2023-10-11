7ceebf1f7f73936f010559956963fb85395ac3a7
Del <deldelnd02@gmail.com>
2023-10-11 23:14:30 +0100

    75% responsive site vitrine

--- front/js/general.js
+++ front/js/general.js
@@ -3,39 +3,57 @@ $(document).ready(async function () {
   displayHeaderFooter();
 });
 
+function checkMobile() {
+  if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function displayHeaderFooter() {
-  $("header").html(`
-    <img src="img/garage-logo.png" alt="logo garage v.parrot" class="brand" />
-    <nav>
-      <a href="index.html" class="nav-item">Accueil</a>
-      <a href="showroom.html" class="nav-item">Showroom</a>
-      <div class="nav-item scrollto" data-id="#contact">Contact</div>
-    </nav>
-  `);
+  let isMobile = checkMobile()
+  if (isMobile) {
+    $("header").html(`
+      <img src="img/garage-logo.png" alt="logo garage v.parrot" class="brand" />
+      <div class="wrapper">
+      <i class="fa-solid fa-bars" id="nav-toggle"></i>
+        <nav class="mobile">
+          <a href="index.html" class="nav-item">Accueil</a>
+          <a href="showroom.html" class="nav-item">Showroom</a>
+          <div class="nav-item scrollto" data-id="#contact">Contact</div>
+        </nav>
+      </div>
+    `);
+  } else {
+    $("header").html(`
+      <img src="img/garage-logo.png" alt="logo garage v.parrot" class="brand" />
+      <nav>
+        <a href="index.html" class="nav-item">Accueil</a>
+        <a href="showroom.html" class="nav-item">Showroom</a>
+        <div class="nav-item scrollto" data-id="#contact">Contact</div>
+      </nav>
+    `);
+  }
   $("footer").html(`
     <img src="img/garage-logo.png" class="brand" />
     <div class="copyright">Copyright 2023 - Garage V. Parrot</div>
     <div class="contacts">
-      <div><span>Telephone : </span>1234567890</div>
-      <div><span>Email : </span>email@email.com</div>
-      <div><span>Addresse : </span>12 rue paris, paris, france</div>
+      <div>1234567890</div>
+      <div>email@email.com</div>
+      <div>12 rue paris, paris, france</div>
     </div>
   `);
 }
 
 $(document).on('click',".scrollto", function () {
   let section = $(this).data("id");
-  let page = '/front/index.html';
   let currentPage = window.location.pathname;
-  console.log(currentPage);
-  if (currentPage != page) {
+  if (currentPage != '/front/index.html') {
     window.location.href = page + section
   }
   let offset = 30;
   $("html, body").animate(
-    {
-      scrollTop: $(section).offset().top - offset,
-    },
-    300
+    {scrollTop: $(section).offset().top - offset},300
   );
 });
