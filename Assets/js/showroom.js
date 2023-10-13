bfca1c36ae5273043c8b339b768d7f046e0b712a
Del <deldelnd02@gmail.com>
2023-10-13 10:33:35 +0100

    ajout e dashboard.js, display showroom cards

--- Assets/js/showroom.js
+++ Assets/js/showroom.js
@@ -1,9 +1,31 @@
 $(document).ready(async function () {
   console.log("showroom.js Loaded");
+  displayCars(50)
 });
 
 $('.card').click(function (e) { 
   e.preventDefault();
   let id = $(this).data("id")
-  window.location.href = '../car-details?id=' +id
+  window.location.href = `../car` //?id=${id}
 });
+
+function displayCars(number) {
+  let i = 0
+  while (i<=number){
+    i++
+    $('#cars-list').append(`
+    <div class="card" data-id="${number}">
+      <img src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
+      <div class="wrapper">
+        <div class="car-name title">Porsche 911</div>
+        <div class="price">20 000 €</div>
+        <div class="details">
+          <div>2020</div>
+          <div class="spacer">•</div>
+          <div>289439 Km</div>
+        </div>
+      </div>
+    </div>
+    `);
+  }
+}
