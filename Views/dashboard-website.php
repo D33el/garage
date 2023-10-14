c6160238c8de0dda0c7800f8b80f7751db4c24cc
Del <deldelnd02@gmail.com>
2023-10-14 09:10:32 +0100

    ajout routes, ajout HTML

--- Views/dashboard-website.php
+++ Views/dashboard-website.php
@@ -1,1 +1,33 @@
 
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Garage V. Parrot</title>
+    <base href="/assets/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/general.css" />
+    <link rel="stylesheet" href="css/dashboard.css" />
+  </head>
+
+  <body>
+    <main id="app">
+      <header class="dashboard"><!-- Display general.js--></header>
+      <div class="sidebar"><!-- Display general.js--></div>
+      <div class="content">
+        <div class="page" id="showroom">
+          <div class="page-header">
+            <div class="wrap">
+              <div class="title">Gestion du site web</div>
+            </div>
+            <button class="primary has-icon" id=""><i class="fa-solid fa-plus"></i>Nouvelle voiture</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="js/general.js"></script>
+  <script src="js/dashboard.js"></script>
+</html>
